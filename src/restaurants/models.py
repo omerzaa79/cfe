1d50bb49dc9956645b8fbefa95bed4f64664ff45
omerzaa79 <omerzaa79@gmail.com>
2018-05-09 16:46:49 +0500

    started Models

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class RestaurantLocation(models.Model):
+	name = models.CharField(max_length=120)
+	location = models.CharField(max_length=120, null=True, blank=True)
+
+	def __str__(self):
+		return self.name + " Restaurant "
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,32 +1,23 @@
 import random
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-# function based view
-def home(request):
-    num = None
-    some_list = [
-        random.randint(0, 100000000), 
-        random.randint(0, 100000000), 
-        random.randint(0, 100000000)
-    ]
-    condition_bool_item = False
-    if condition_bool_item:
-        num = random.randint(0, 100000000)
-    context = {
-        "num": num, 
-        "some_list": some_list
-    }
-    return render(request, "home.html", context)
-
-def about(request):
-    context = {
-    }
-    return render(request, "about.html", context)
+from django.views.generic import TemplateView
 
 
-def contact(request):
-    context = {
-    }
-    return render(request, "contact.html", context)
+class HomeView(TemplateView):
+    template_name = 'home.html' 
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = None
+        some_list = [
+            random.randint(0, 100000000), 
+            random.randint(0, 100000000), 
+            random.randint(0, 100000000)
+        ]
+        condition_bool_item = False
+        if condition_bool_item:
+            num = random.randint(0, 100000000)
+        context = {
+            "num": num, 
+            "some_list": some_list
+        }
+        return context
